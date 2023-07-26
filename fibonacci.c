b3bfcfc0cb97a9ebeb30cf3708f3fda04b05334e
Jameel Ahmed. J <123755027+Jameel-Ahmed-2003@users.noreply.github.com>
2023-07-26 19:58:19 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,30 @@
+/* Recursion fibonacci series*/
+#include<stdio.h>
+#include<windows.h>
+int n,i=0,a=0,b=1,c;
+int fibo(int i)
+{
+    if(i<n)
+    {
+        if(i==a||i==b)
+        {
+            if(i==a)
+            printf("%d ",a);
+            else if(i==b)
+            printf("%d ",b);
+            i++;
+        }
+        c=a+b;
+        printf("%d ",c);
+        i++;
+        a=b,b=c;
+        fibo(i);
+    }
+}
+void main()
+{
+    system("cls");
+    printf("Number of terms ");
+    scanf("%d",&n);
+    fibo(i);
+}
